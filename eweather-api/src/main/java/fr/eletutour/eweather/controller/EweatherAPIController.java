9fcdd612b3c5dd7b546a1e155c9ab47df29cb7d2
Erwan <erwanletutour.elt@gmail.com>
2020-10-08 14:22:31 +0200

    allow cors for ihm developpement

--- eweather-api/src/main/java/fr/eletutour/eweather/controller/EweatherAPIController.java
+++ eweather-api/src/main/java/fr/eletutour/eweather/controller/EweatherAPIController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ewanletutour
@@ -41,6 +38,7 @@ public class EweatherAPIController {
             @ApiResponse(code = 500, message = "An error occured")
     }
     )
+    @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080"}, allowedHeaders = "*")
     public ResponseEntity getWeather(@RequestParam(name = "location") String location) throws Exception {
         return new ResponseEntity(weatherService.getForecast(location), HttpStatus.OK);
     }
